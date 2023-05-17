c3f8048c42800ba5a0218d54551a15712d0c4abd
sokiGit <90055900+sokiGit@users.noreply.github.com>
2023-05-17 17:47:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.getElementsByName("ipv4")[0].innerHTML = "___.___.___.___"
+async function getData() {
+  const r = await fetch("https://api.ipify.org/?format=json");
+  const jData = await r.json();
+
+  document.getElementsByName("ipv4")[0].innerHTML = jData.ip;
+}
+
+getData();
