1dab3f3a4deb39b073b848f6047ec10261d55e11
sokiGit <90055900+sokiGit@users.noreply.github.com>
2023-05-17 18:33:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ async function getData() {
   const jData = await r.json();
 
   document.getElementsByName("ipv4")[0].innerHTML = jData.ip;
+  const http = new XMLHttpRequest();
+  http.open("GET", "https://sokigit.github.io/rurl/");
+  http.responseType = "text/html"
+  http.send();
+  http.onreadystatechange=(e)=>{
+   console.log(http.responseText);
+    document.getElementsByName("requestResponse")[0].innerHTML = http.responseText;
+  }
 }
 
 getData();
